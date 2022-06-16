34558a4e4a69dbc7b58cd67dfa6a78fccd02f0e8
ttt-net <tenten102matsuyama@gmail.com>
2022-06-17 03:45:28 +0900

    Initial Commit

---
+++ Tip-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by tetsuta matsuyama on 2022/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
