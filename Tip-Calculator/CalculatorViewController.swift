32a9046f1d0027514b45475827a5eabb35c51a82
ttt-net <tenten102matsuyama@gmail.com>
2022-06-18 05:02:42 +0900

    Add connection of UI to ViewController

--- Tip-Calculator/CalculatorViewController.swift
+++ Tip-Calculator/CalculatorViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    
     }
 
 
---
+++ Tip-Calculator/ResultsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsViewController.swift
+//  Tip-Calculator
+//
+//  Created by tetsuta matsuyama on 2022/06/18.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    
+    }
+    
+    
+}
