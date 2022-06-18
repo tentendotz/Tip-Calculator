5e98b682e54d2d7c88b94e3120e0bc6cfaaac835
ttt-net <tenten102matsuyama@gmail.com>
2022-06-19 03:54:23 +0900

    Add feature to calculate the tip

--- Tip-Calculator/CalculatorViewController.swift
+++ Tip-Calculator/CalculatorViewController.swift
@@ -14,11 +14,11 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    
+    var tipTitle = "10%"
+    var tipNumber = 0.10
+    var numberOfPeople = 2
+    var finalResult = "0.0"
     
     @IBAction func tipChanged(_ sender: UIButton) {
         
@@ -29,13 +29,27 @@ class CalculatorViewController: UIViewController {
         twentyPctButton.isSelected = false
         sender.isSelected = true
         
+        tipTitle = sender.currentTitle!
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
         splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let bill = Double(billTextField.text!)
+        
+        if let safeBill = bill {
+            let buttonTitleMinusPercentSign = Double(tipTitle.dropLast())!
+            tipNumber = buttonTitleMinusPercentSign / 100
+            let result = safeBill * (1 + tipNumber) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+            
+        }
+        
     
     }
 
